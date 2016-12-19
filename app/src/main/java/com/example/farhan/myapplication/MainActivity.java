3d660ec3b08a787573fd16a02f82f84e83642ee3
FarhanGhaffar76 <farhanghaffar76@yahoo.com>
2016-12-19 22:47:41 +0500

    declare int type variable

--- app/src/main/java/com/example/farhan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/farhan/myapplication/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         int a=1;
         int b = 1;
-        int c = -1;
+        int c = 0;
     }
 }
